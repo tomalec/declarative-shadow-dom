950bdc529e67b64e1190a9749a7b54a59213cdac
Tomek Wytrębowicz <tomalecpub@gmail.com>
2017-11-05 18:04:25 +0100

    Initial implementation of polyfill for Apple's proposal

--- declarative-shadow-dom.js
+++ declarative-shadow-dom.js
@@ -1,55 +1,16 @@
 customElements.define('declarative-shadow-dom', class extends HTMLTemplateElement {
     static get observedAttributes() {
-        return ['auto-stamp', 'stamped', 'disabled'];
+        return [];
     }
 
     constructor(self) {
         // assignment required by polyfill
         self = super(self);
-        console.info('constructor', this.content, this.parentElement);
-
-        // this.appendToParentsShadowRoot();
     }
     connectedCallback(){
-        // debugger
-    console.info('connectedCallback', this.content, this.parentElement);
-        // if(this.hasAttribute('auto-stamp')){
-        //     this.appendToParentsShadowRoot();
-        // }
-        // debugger
-        if(!this.hasAttribute('disabled')){
-            this.setAttribute('stamped', ''); // this.clear();
-        }
-    }
-    disconnectedCallback(){
-    console.info('disconnectedCallback', this.content, this.parentElement);
-        this.removeAttribute('stamped'); // this.clear();
-    }
-    attributeChangedCallback(name, oldVal, newVal){
-    console.info('attributeChangedCallback', this.content, this.parentElement, this.isConnected, name, oldVal, newVal);
-        switch(name){
-            case 'disabled':
-                if(this.hasAttribute('disabled')){
-                    // this.clear();
-                } else {
-                    if(this.isConnected){
-                        this.appendToParentsShadowRoot();
-                    }
-                }
-                break;
-            case 'stamped':
-                if(this.hasAttribute('stamped')){
-                    if(!this.hasAttribute('disabled')){
-                        this.appendToParentsShadowRoot();
-                    }
-                } else {
-                    this.clear();
-                }
-                break;
-        }
+        this.appendToParentsShadowRoot();
     }
     appendToParentsShadowRoot(){
-        console.info('stamping dsd');
         const parentElement = this.parentElement;
         if(!parentElement){
             throw 'declarative-shadow-dom needs a perentElement to stamp to';
@@ -60,19 +21,10 @@ customElements.define('declarative-shadow-dom', class extends HTMLTemplateElemen
         let fragment = document.importNode(this.content, true);
         this.stampedNodes = Array.prototype.slice.call(fragment.childNodes);
         parentElement.shadowRoot.appendChild(fragment);
-        debugger
+        // debugger
         ShadyCSS && ShadyCSS.styleElement(parentElement);
         parentElement.dispatchEvent(new CustomEvent('declarative-shadow-dom-stamped', {detail: {stampedNodes: this.stampedNodes}}));
-        this.setAttribute('stamped', '');
-    }
-    clear(){
-        console.info('clearing dsd');
-        if(this.stampedNodes){
-            let node;
-            while(node = this.stampedNodes.pop()){
-                node.parentNode && node.parentNode.removeChild(node);
-            }
-        }
+        this.parentNode.removeChild(this);
     }
 }, {
     extends: 'template'
